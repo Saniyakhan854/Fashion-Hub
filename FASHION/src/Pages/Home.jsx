41c2ac38c3bba559d68ac0529d7dea60722286f0
Saniya Khan <saniyakhanmalwan1@gmail.com>
2025-09-02 15:18:43 +0530

    new changes

--- FASHION/src/Pages/Home.jsx
+++ FASHION/src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from '../Common/Footer'
 export default function Home() {
   return (
     <>
-        <h1 className='text-7xl text-center'>Welcome to Home page</h1>
+        <h1 className='text-7xl text-center'>Welcome to Home page.</h1>
           <div className="p-6">
       <h2 className="text-3xl font-bold">Home Page</h2>
       <p className="mt-4 text-gray-700">Welcome to our Mini Project website 🚀</p>
